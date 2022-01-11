f2cc6fe3d9af9fbdd95abba053d3e7f312a3de5d
unknown <dinh.kawa.98@gmail.com>
2022-01-11 19:26:19 +0700

    create city done

---
+++ src/main/java/com/m6findjobbackend/controller/CityController.java
@@ -0,0 +1,22 @@
+package com.m6findjobbackend.controller;
+
+import com.m6findjobbackend.model.City;
+import com.m6findjobbackend.service.city.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/cities")
+public class CityController {
+    @Autowired
+    private CityService cityService;
+
+    @PostMapping("/createCity")
+    public ResponseEntity<City> createCity(@RequestBody City city){
+        return new ResponseEntity<>(cityService.save(city), HttpStatus.CREATED);
+    }
+
+}
