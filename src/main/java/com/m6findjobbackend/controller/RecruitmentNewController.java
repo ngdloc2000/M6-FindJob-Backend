01e1fd6277114248c8ee5a830d71a80ad1fd4509
Nguyễn Tiến Long <email ng k git>
2022-01-12 16:16:43 +0700

    recruitment

--- src/main/java/com/m6findjobbackend/controller/RecruitmentNewController.java
+++ src/main/java/com/m6findjobbackend/controller/RecruitmentNewController.java
@@ -1,6 +1,7 @@
 package com.m6findjobbackend.controller;
 
 import com.m6findjobbackend.dto.response.ResponseMessage;
+import com.m6findjobbackend.model.Company;
 import com.m6findjobbackend.model.RecuitmentNew;
 import com.m6findjobbackend.model.Status;
 import com.m6findjobbackend.service.city.CityService;
@@ -15,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
@@ -39,7 +41,7 @@ public class RecruitmentNewController {
 //    CityService cityService;
 
     @GetMapping("/list")
-    public ResponseEntity<?> showListRecruitmentNew(){
+    public ResponseEntity<?> showListRecruitmentNew() {
         List<RecuitmentNew> recuitmentNewList = (List<RecuitmentNew>) recruitmentNewService.findAll();
         if (recuitmentNewList.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -48,7 +50,7 @@ public class RecruitmentNewController {
     }
 
     @PostMapping
-    public ResponseEntity<?> createRecruitmentNew(@RequestBody RecuitmentNew recuitmentNew){
+    public ResponseEntity<?> createRecruitmentNew(@RequestBody RecuitmentNew recuitmentNew) {
         if (recuitmentNew.getQuantity() == Integer.parseInt(null)) {
             return new ResponseEntity<>(new ResponseMessage("no_quantity"), HttpStatus.OK);
         }
@@ -61,6 +63,50 @@ public class RecruitmentNewController {
 
         recruitmentNewService.save(recuitmentNew);
         return new ResponseEntity<>(new ResponseMessage("yes"), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detailRecruitmentNew(@PathVariable Long id) {
+        Optional<RecuitmentNew> recuitmentNew = recruitmentNewService.findById(id);
+        if (!recuitmentNew.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(recuitmentNew, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteRecruitmentNew(@PathVariable Long id) {
+        Optional<RecuitmentNew> recuitmentNew = recruitmentNewService.findById(id);
+        if (!recuitmentNew.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        recruitmentNewService.deleteById(id);
+        return new ResponseEntity<>(new ResponseMessage("yes"), HttpStatus.OK);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateRecruitmentNew(@PathVariable Long id, @RequestBody RecuitmentNew recuitmentNew) {
+        Optional<RecuitmentNew> recuitmentNew1 = recruitmentNewService.findById(id);
+        if (!recuitmentNew1.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        if (recuitmentNew.getQuantity() == Integer.parseInt(null)) {
+            return new ResponseEntity<>(new ResponseMessage("no_quantity"), HttpStatus.OK);
+        }
+        recuitmentNew1.get().setTitle(recuitmentNew.getTitle());
+        recuitmentNew1.get().setWorkingTime(recuitmentNew.getWorkingTime());
+        recuitmentNew1.get().setField(recuitmentNew.getField());
+        recuitmentNew1.get().setVacancies(recuitmentNew.getVacancies());
+        recuitmentNew1.get().setExpDate(recuitmentNew.getExpDate());
+        recuitmentNew1.get().setDescription(recuitmentNew.getDescription());
+        recuitmentNew1.get().setQuantity(recuitmentNew.getQuantity());
+        recuitmentNew1.get().setGender(recuitmentNew.getGender());
+        recuitmentNew1.get().setCity(recuitmentNew.getCity());
+        recruitmentNewService.save(recuitmentNew1.get());
+        return new ResponseEntity<>(new ResponseMessage("yes"), HttpStatus.OK);
+
     }
 
 }
